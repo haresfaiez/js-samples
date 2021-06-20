8d827d6a28d9a1d5e4457c8ba4fa9aea958f52b9
faiez <felhares@iobeya.com>
2021-06-20 22:29:34 +0100

    Add nested calls to two-modules-input

--- two-modules-input/index.js
+++ two-modules-input/index.js
@@ -2,7 +2,7 @@ var Sum = require('./sum');
 
 
 function findSum() {
-  const result = Sum.sum(1, 9);
+  const result = Sum.sum(1, 9) + Sum.sum(20, 22);
   return result;
 }
 
--- two-modules-input/sum.js
+++ two-modules-input/sum.js
@@ -1,5 +1,15 @@
+function increment(x) {
+  let result = x + 1;
+  return result;
+}
+
+function augment(it) {
+  let result = increment(it) + 1;
+  return result;
+}
+
 function sum(a, b) {
-  let result = a + b;
+  let result = augment(a) + augment(b);
   return result;
 }
 
